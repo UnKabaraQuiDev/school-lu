ddf8c2f66322f902638cd638b41edf9790c3958a
UnKabaraQuiDev <u.kbra.lu@gmail.com>
2026-08-29 23:28:46 +0200

    moved db rules to pclib

--- src/main/java/lu/kbra/school_lu/config/DbConfig.java
+++ src/main/java/lu/kbra/school_lu/config/DbConfig.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
-import lu.kbra.pclib.db.hook.VersionDbRule;
+import lu.kbra.pclib.db.hook.VersionRule;
+import lu.kbra.pclib.db.rule.TraceRule;
+import lu.kbra.pclib.db.rule.ValidationRule;
 import lu.kbra.pclib.db.utils.DatabaseQueryableHookTemplate;
 import lu.kbra.pclib.db.utils.impl.SQLQueryableRule;
+import lu.kbra.pclib.db.validation.TableValidatorFactory;
 
 @Configuration
 public class DbConfig {
@@ -22,8 +26,22 @@ public class DbConfig {
 
 	@Bean
 	@Order(10)
-	VersionDbRule versionDbRule() {
-		return new VersionDbRule(true);
+	VersionRule versionRule() {
+		return new VersionRule(true);
+	}
+
+	@Bean
+	@Order(5)
+	@Profile("debug")
+	TraceRule traceDbRule() {
+		return new TraceRule();
+	}
+
+	@Bean
+	@Order(5)
+	@Profile("debug")
+	ValidationRule validationDbRule(TableValidatorFactory tvf) {
+		return new ValidationRule(tvf);
 	}
 
 }
