f90ee76f83d53228e41971ca4e2546631151bdc2
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-09-07 20:40:03 +0200

    fix: a lot of things

--- src/main/java/lu/kbra/school_lu/db/table/TagTable.java
+++ src/main/java/lu/kbra/school_lu/db/table/TagTable.java
@@ -5,10 +5,14 @@ import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
+import lu.kbra.pclib.db.annotations.entry.Column;
+import lu.kbra.pclib.db.annotations.query.Any;
 import lu.kbra.pclib.db.annotations.query.Param;
 import lu.kbra.pclib.db.annotations.query.Query;
+import lu.kbra.pclib.db.annotations.view.SelectColumn;
 import lu.kbra.pclib.db.annotations.view.Table;
 import lu.kbra.pclib.db.base.DeferredDatabase;
+import lu.kbra.pclib.db.impl.DatabaseEntry.ReadOnlyDatabaseEntry;
 import lu.kbra.pclib.db.table.DeferredDatabaseTable;
 import lu.kbra.school_lu.db.data.ExerciseData;
 import lu.kbra.school_lu.db.data.TagData;
@@ -28,5 +32,15 @@ public abstract class TagTable extends DeferredDatabaseTable<TagData> {
 
 	@Query(tables = { @Table(typeName = ExerciseTagTable.class), @Table(typeName = ExerciseTable.class) })
 	public abstract List<TagData> byExercise(@Param ExerciseData c);
+
+	@Query(
+			retColumns = { "{M:TagTable:id}", "{M:TagTable:name}" },
+			tables = { @Table(columns = { @SelectColumn(func = "count(*)", asName = "count") }, typeName = ExerciseTagTable.class) },
+			groupBy = { "{M:TagTable:id}" }
+	)
+	public abstract List<TagProp> propByName(@Param @Any Set<String> name);
+
+	public record TagProp(@Column int id, @Column String name, @Column int count) implements ReadOnlyDatabaseEntry {
+	}
 
 }
