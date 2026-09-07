4f14ea278b426919fb16860bfda991278935edc1
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-09-07 15:38:42 +0200

    added byAnySubjectAllTagsNotByStatus withSolutionAnySubjectAllTagsNotByStatus byAnySubjectAllTags

--- src/main/java/lu/kbra/school_lu/db/table/ExamAttachmentTable.java
+++ src/main/java/lu/kbra/school_lu/db/table/ExamAttachmentTable.java
@@ -2,9 +2,13 @@ package lu.kbra.school_lu.db.table;
 
 import org.springframework.stereotype.Component;
 
+import lu.kbra.pclib.db.annotations.query.Param;
+import lu.kbra.pclib.db.annotations.query.Query;
+import lu.kbra.pclib.db.annotations.view.Table;
 import lu.kbra.pclib.db.base.DeferredDatabase;
 import lu.kbra.pclib.db.table.DeferredDatabaseTable;
 import lu.kbra.school_lu.db.data.ExamAttachmentData;
+import lu.kbra.school_lu.db.data.ExerciseData;
 
 @Component
 public abstract class ExamAttachmentTable extends DeferredDatabaseTable<ExamAttachmentData> {
@@ -12,5 +16,13 @@ public abstract class ExamAttachmentTable extends DeferredDatabaseTable<ExamAtta
 	public ExamAttachmentTable(final DeferredDatabase database) {
 		super(database);
 	}
+
+	@Query(
+			distinct = true,
+			retColumns = { "{M:ExamAttachmentTable:name}" },
+			tables = { @Table(typeName = ExerciseAttachmentTable.class), @Table(typeName = ExerciseTable.class) },
+			strategy = Query.Type.SINGLE_NULL
+	)
+	public abstract String nameByExerciseAttachment(@Param ExerciseData c);
 
 }
--- src/main/java/lu/kbra/school_lu/db/table/ExerciseTable.java
+++ src/main/java/lu/kbra/school_lu/db/table/ExerciseTable.java
@@ -7,13 +7,17 @@ import org.springframework.stereotype.Component;
 
 import lu.kbra.pclib.db.annotations.query.All;
 import lu.kbra.pclib.db.annotations.query.Limit;
+import lu.kbra.pclib.db.annotations.query.OrIsNull;
 import lu.kbra.pclib.db.annotations.query.Param;
 import lu.kbra.pclib.db.annotations.query.Query;
+import lu.kbra.pclib.db.annotations.view.SelectColumn;
 import lu.kbra.pclib.db.annotations.view.Table;
 import lu.kbra.pclib.db.base.DeferredDatabase;
 import lu.kbra.pclib.db.table.DeferredDatabaseTable;
+import lu.kbra.school_lu.data.ExerciseStatus;
 import lu.kbra.school_lu.db.data.ExerciseData;
 import lu.kbra.school_lu.db.data.SubjectData;
+import lu.kbra.school_lu.db.data.UserData;
 
 @Component
 public abstract class ExerciseTable extends DeferredDatabaseTable<ExerciseData> {
@@ -41,11 +45,16 @@ public abstract class ExerciseTable extends DeferredDatabaseTable<ExerciseData> 
 	@Query(
 			distinct = true,
 			tables = {
-					@Table(typeName = ExamTable.class),
+					@Table(typeName = ExerciseAttachmentTable.class),
+					@Table(columns = { @SelectColumn(name = "name") }, typeName = ExamAttachmentTable.class),
+					@Table(typeName = ExamTable.class, on = "{M:ExamAttachmentTable:examId} = {M:ExamTable:id}"),
 					@Table(typeName = SubjectTable.class),
 					@Table(typeName = ExerciseTagTable.class),
-					@Table(typeName = TagTable.class) },
+					@Table(typeName = TagTable.class)
+
+			},
 			condition = ExerciseTable.hasSolutionAndStatement
+//			groupBy = { "{M:ExerciseTable:id}", "{M:ExamAttachmentTable:name}" }
 	)
 	public abstract List<ExerciseData> withSolutionAnySubjectAnyTag(
 			@Param Collection<SubjectData> subjects,
@@ -54,21 +63,80 @@ public abstract class ExerciseTable extends DeferredDatabaseTable<ExerciseData> 
 	@Query(
 			distinct = true,
 			tables = {
-					@Table(typeName = ExamTable.class),
+					@Table(typeName = ExerciseAttachmentTable.class),
+					@Table(columns = { @SelectColumn(name = "name") }, typeName = ExamAttachmentTable.class),
+					@Table(typeName = ExamTable.class, on = "{M:ExamAttachmentTable:examId} = {M:ExamTable:id}"),
 					@Table(typeName = SubjectTable.class),
 					@Table(typeName = ExerciseTagTable.class),
 					@Table(typeName = TagTable.class)
-			/*
-			 * @Table(typeName = SubjectTable.class, on = "{M:ExamTable:subjectId} = {M:SubjectTable:id}"),
-			 *
-			 * @Table(typeName = SectionTable.class, on = "{M:SubjectTable:sectionId} = {M:SectionTable:id}")
-			 */
+
 			},
 			condition = ExerciseTable.hasSolutionAndStatement
+//			groupBy = { "{M:ExerciseTable:id}", "{M:ExamAttachmentTable:name}" }
 	)
 	public abstract List<ExerciseData> withSolutionAnySubjectAllTags(
 			@Param Collection<SubjectData> subjects,
 			@All @Param(value = "{M:TagTable:name}", ignoreNull = true) Collection<String> tagData,
+			@Limit int limit);
+
+	@Query(
+			distinct = true,
+			tables = {
+					@Table(typeName = ExerciseAttachmentTable.class),
+					@Table(columns = { @SelectColumn(name = "name") }, typeName = ExamAttachmentTable.class),
+					@Table(typeName = ExamTable.class, on = "{M:ExamAttachmentTable:examId} = {M:ExamTable:id}"),
+					@Table(typeName = SubjectTable.class),
+					@Table(typeName = ExerciseTagTable.class),
+					@Table(typeName = TagTable.class)
+
+			}
+//			groupBy = { "{M:ExerciseTable:id}", "{M:ExamAttachmentTable:name}" }
+	)
+	public abstract List<ExerciseData> byAnySubjectAllTags(
+			@Param Collection<SubjectData> subjects,
+			@All @Param(value = "{M:TagTable:name}", ignoreNull = true) Collection<String> tagData,
+			@Limit int limit);
+
+	@Query(
+			distinct = true,
+			tables = {
+					@Table(typeName = ExerciseAttachmentTable.class),
+					@Table(columns = { @SelectColumn(name = "name") }, typeName = ExamAttachmentTable.class),
+					@Table(typeName = ExamTable.class, on = "{M:ExamAttachmentTable:examId} = {M:ExamTable:id}"),
+					@Table(typeName = SubjectTable.class),
+					@Table(typeName = ExerciseTagTable.class),
+					@Table(typeName = TagTable.class),
+					@Table(typeName = UserExerciseTable.class, join = Table.Type.LEFT),
+					@Table(typeName = UserTable.class, join = Table.Type.LEFT) },
+			condition = hasSolutionAndStatement
+//			groupBy = { "{M:ExerciseTable:id}", "{M:ExamAttachmentTable:name}" }
+	)
+	public abstract List<ExerciseData> withSolutionAnySubjectAllTagsNotByStatus(
+			@Param Collection<SubjectData> subjects,
+			@All @Param(value = "{M:TagTable:name}", ignoreNull = true) Collection<String> tagData,
+			@Param @OrIsNull UserData userData,
+			@Param(comparator = "IS DISTINCT FROM") ExerciseStatus status,
+			@Limit int limit);
+
+	@Query(
+			distinct = true,
+			tables = {
+					@Table(typeName = ExerciseAttachmentTable.class),
+					@Table(columns = { @SelectColumn(name = "name") }, typeName = ExamAttachmentTable.class),
+					@Table(typeName = ExamTable.class, on = "{M:ExamAttachmentTable:examId} = {M:ExamTable:id}"),
+					@Table(typeName = SubjectTable.class),
+					@Table(typeName = ExerciseTagTable.class),
+					@Table(typeName = TagTable.class),
+					@Table(typeName = UserExerciseTable.class, join = Table.Type.LEFT),
+					@Table(typeName = UserTable.class, join = Table.Type.LEFT) }
+//			groupBy = { "{M:ExerciseTable:id}", "{M:ExamAttachmentTable:name}" }
+//			condition = "{M:UserExerciseTable:status} NOT IN {V:status} OR {M:UserExerciseTable:status} IS NULL"
+	)
+	public abstract List<ExerciseData> byAnySubjectAllTagsNotByStatus(
+			@Param Collection<SubjectData> subjects,
+			@All @Param(value = "{M:TagTable:name}", ignoreNull = true) Collection<String> tagData,
+			@Param @OrIsNull UserData userData,
+			@Param(comparator = "IS DISTINCT FROM") ExerciseStatus status,
 			@Limit int limit);
 
 }
