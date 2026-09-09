d9702c955273519cc2622b8b107ce0c2b953c7f5
UnKabaraQuiDev <u.kbra.lu@gmail.com>
2026-09-09 11:47:22 +0200

    fix: exam/exam-part by attachment for parent

--- src/main/java/lu/kbra/school_lu/db/table/ExamAttachmentTable.java
+++ src/main/java/lu/kbra/school_lu/db/table/ExamAttachmentTable.java
@@ -2,9 +2,9 @@ package lu.kbra.school_lu.db.table;
 
 import org.springframework.stereotype.Component;
 
+import lu.kbra.pclib.db.annotations.entry.Nullable;
 import lu.kbra.pclib.db.annotations.query.Param;
 import lu.kbra.pclib.db.annotations.query.Query;
-import lu.kbra.pclib.db.annotations.query.Query.Type;
 import lu.kbra.pclib.db.annotations.view.Table;
 import lu.kbra.pclib.db.base.DeferredDatabase;
 import lu.kbra.pclib.db.table.DeferredDatabaseTable;
@@ -24,12 +24,11 @@ public abstract class ExamAttachmentTable extends DeferredDatabaseTable<ExamAtta
 			tables = {
 					@Table(typeName = ExamPartTable.class),
 					@Table(typeName = ExamPartExamTable.class),
-					@Table(typeName = ExamTable.class) },
-			strategy = Type.SINGLE_THROW
+					@Table(typeName = ExamTable.class) }
 	)
 	public abstract ExamAttachmentData byExamAndPartNameAndQualifier(
 			@Param ExamData exam,
-			@Param("{M:ExamPartTable:name}") String partName,
+			@Param("{M:ExamPartTable:name}") @Nullable String partName,
 			@Param ExamAttachmentType qualifier);
 
 }
